2359ecb1ecbdca30148c73571b2f405a68971728
chitra <chmanu.chinu@gmail.com>
2023-04-14 13:17:49 +0000

    tf-module-docdb

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "aws_docdb_cluster" "main" {
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = true
- //db_subnet_group_name = aws_docdb_subnet_group.main.name
-  //kms_key_id = data.aws_kms_key.key.key_id.arn
-  //storage_encrypted = true
+  db_subnet_group_name = aws_docdb_subnet_group.main.name
+  kms_key_id = data.aws_kms_key.key.key_id.arn
+  storage_encrypted = true
 }
 
 resource "aws_docdb_subnet_group" "main" {
